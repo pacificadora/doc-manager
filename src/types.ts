6e7682aa330a86ef7b1e8b48c0b99718ee952c4b
Aman Singhal <singhalaman198@gmail.com>
2025-06-11 08:09:58 +0530

    created the constructor type for the decorators

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Constructor<T = any, Arguments extends unknown[] = any[]> = new (
+    ...arguments_: Arguments
+) => T;
+
+export type KeyOfType<Entity, U> = {
+    [P in keyof Required<Entity>]: Required<Entity>[P] extends U
+    ? P
+    : Required<Entity>[P] extends U[]
+    ? P
+    : never;
+}[keyof Entity];
+
+export type Reference<T> = T;
